7b5597c72cd4bc48f8cbbee32e19af1115b1f930
NikitaDonkey <polovykhnikita@gmail.com>
2024-03-12 00:35:44 +0700

    first another branch exp

---
+++ WinFormsGid/Data/Entities/Material.cs
@@ -0,0 +1,10 @@
+﻿namespace WinFormsApp.Data.Entities;
+
+internal class Material
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+
+
+	public IEnumerable<Product> Products { get; set; } = null!;
+}
---
+++ WinFormsGid/Forms/ProductsCustomForm.cs
@@ -0,0 +1,28 @@
+﻿using WinFormsApp.Controls;
+using WinFormsApp.Services.Interfaces;
+
+namespace WinFormsApp.Forms;
+
+internal partial class ProductsCustomForm : Form
+{
+	private readonly IDbWorker _db;
+
+	public ProductsCustomForm(IDbWorker db)
+	{
+		InitializeComponent();
+		_db = db;
+	}
+
+	private void ProductsDataGridForm_Load(object sender, EventArgs e)
+	{
+		foreach (var item in _db.Products)
+		{
+			flpnlProducts.Controls.Add(new ProductView(_db, item));
+		}
+	}
+
+	private void btnSave_Click(object sender, EventArgs e)
+	{
+		_db.SaveChanges();
+	}
+}
