cc78038ddae133be2fe9fad5b7e438d4cac225c7
NikitaDonkey <polovykhnikita@gmail.com>
2024-03-12 00:46:15 +0700

    new exp with git

---
+++ CodeFirstExistingDatabaseSample/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirstExistingDatabaseSample.Models
+{
+    public partial class Blog
+    {
+        public Blog()
+        {
+            Posts = new HashSet<Post>();
+        }
+
+        public int BlogId { get; set; }
+        public string? Name { get; set; }
+        public string? Url { get; set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ CodeFirstExistingDatabaseSample/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeFirstExistingDatabaseSample.Models
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public int BlogId { get; set; }
+
+        public virtual Blog Blog { get; set; } = null!;
+    }
+}
