03e793303f54da92e8cf0761bdc37df6564cae73
NikitaDonkey <polovykhnikita@gmail.com>
2024-03-30 05:39:42 +0700

    return to your owner

--- application_win_form_db/Services/GlobalData.cs
+++ application_win_form_db/Services/GlobalData.cs
@@ -8,7 +8,7 @@
 	}
 
 	//string userInput = Interaction.InputBox("Please enter your input:", "User Input", "");
-	//MessageBox.Show("You entered: " + userInput);
+	//MessageBox.Show("You entered: " + userInput);	//create the stupid dialog form
 
 	public static class GlobalData
 	{
